8cb53f6577b033ee66abbcf4f45587cb65ad9143
Paul John <rainson.work@gmail.com>
2024-03-08 14:09:51 +0300

    comment template

---
+++ docs/template.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+################################################################################
+# Script Name: my_script.sh
+# Description: (Briefly describe what this script does)
+#
+# Author: Your Name
+# Date: (Date of creation or last modification)
+#
+# Usage: ./my_script.sh [options]
+#
+# Options:
+#   -h, --help     Display this help message and exit
+#   -o, --option   Some option description
+#
+# Example Usage:
+#   ./my_script.sh -o value
+#
+# Notes:
+#   - Add any additional notes or important information here.
+#   - Use a bulleted list for clarity.
+#
+################################################################################
